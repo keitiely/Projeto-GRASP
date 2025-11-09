0062be438f74c896feba72967b86aa1469cb3c5c
Keitiely Silva <keitielysilvaviana@gmail.com>
2025-11-09 01:55:46 -0300

    Completa a classe Usuario (construtor e métodos)

--- ProjetoGRASP/src/main/java/Model/Usuario.java
+++ ProjetoGRASP/src/main/java/Model/Usuario.java
@@ -8,10 +8,43 @@ package Model;
  *
  * @author keity
  */
-public class  {
+public class Usuario {
+ /**
+ * Princípio GRASP aplicado: Alta Coesão (High Cohesion)
+ *
+ * Assim como a classe Livro, a classe Usuario demonstra Alta Coesão.
+ * Sua única responsabilidade é gerenciar os dados
+ * de um usuário da biblioteca.
+ *
+ * Ela não se preocupa em como um livro é emprestado ou como a
+ * interface funciona. Ela apenas guarda informações sobre o usuário.
+ * Isso a torna simples, focada e fácil de manter.
+ */
+    
+    private String nome;
+    private String matricula;
     
     
-    private nome String;
-    private 
+    public Usuario(String nome, String matricula){
+         this.nome = nome;
+         this.matricula = matricula;  
+    }
     
+    
+   // Getters e Setters 
+    public String getNome(){
+        return nome;
+    }
+    
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public String getMatricula(){
+        return matricula;
+    }
+    
+    public void setMatricula(String matricula){
+        this.matricula = matricula;
+    }
 }
